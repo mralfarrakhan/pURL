cd4a1ff2067fa9f96fbf011a2877db572ec0847e
Muhammad Rafky <muhammad.rafky@amartha.com>
2026-06-15 10:47:37 +0700

    prepare for db

--- purl-bin/src/main.rs
+++ purl-bin/src/main.rs
@@ -3,15 +3,19 @@
 mod components;
 mod purl;
 
+use std::error::Error;
+
 use crate::purl::Purl;
 use eframe::{NativeOptions, run_native};
 
-fn main() -> eframe::Result {
+fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
 
     let options = NativeOptions {
         ..Default::default()
     };
 
-    run_native("pURL", options, Box::new(|cc| Ok(Box::new(Purl::new(cc)))))
+    run_native("pURL", options, Box::new(|cc| Ok(Box::new(Purl::new(cc)))))?;
+
+    Ok(())
 }
